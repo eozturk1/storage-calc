10468f2168ea04d4b38b2eb7ffa688a6b41b2bc9
Ercan Ozturk <eoz@fb.com>
2022-07-29 20:34:21 -0700

    Add plot script for large batch

---
+++ scripts/large_batch/plot_large_batch.py
@@ -0,0 +1,69 @@
+from itertools import cycle
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("ozks_memory_long.csv", sep=",", header=None)
+values = df.values
+
+keys = [int((int(ks) / 100_000)) for ks in values[:, 0]]
+times = values[:, 2]
+history = values[:, 3] / (2.0**10)
+users = values[:, 4] / (2.0**10)
+ozks = values[:, 5] / (2.0**10)
+total = history + users + ozks
+
+plt.figure(figsize=(6.4, 2.4))
+markers = cycle(["o", "v", "s", "p", "D", "P"])
+
+
+plt.errorbar(
+    keys,
+    total,
+    label="Total",
+    linestyle="dotted",
+    marker=next(markers),
+    capsize=3,
+)
+
+plt.errorbar(
+    keys,
+    history,
+    label="History",
+    linestyle="dotted",
+    marker=next(markers),
+    capsize=3,
+)
+
+plt.errorbar(
+    keys,
+    users,
+    label="Users",
+    linestyle="dotted",
+    marker=next(markers),
+    capsize=3,
+)
+
+plt.errorbar(
+    keys,
+    ozks,
+    label="Ozks",
+    linestyle="dotted",
+    marker=next(markers),
+    capsize=3,
+)
+
+
+plt.legend(loc="upper left", ncol=2)
+plt.xlim(xmin=0)
+plt.ylim(bottom=0, top=None)
+plt.xlabel("Number of keys (100K)", fontweight="bold")
+plt.ylabel("Parakeet Storage (GB)", fontweight="bold")
+plt.xticks(weight="bold")
+plt.yticks(weight="bold")
+# ax = plt.gca()
+# ax.set_xlim(x_seemless[0], x_seemless[-1])
+# plt.xticks(x_seemless, weight="bold")
+# plt.yticks(y_seemless + [y_parakeet[-1]], weight="bold")
+plt.grid()
+plt.savefig("ozks_memory_long.pdf", bbox_inches="tight")
